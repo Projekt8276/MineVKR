f51f7d5372efd990e8bdecd8d38b399de7234b30
helixd-2k18 <helixd.2k18@gmail.com>
2020-05-30 15:22:16 +0800

    native library merged into main JiviX project

--- src/main/java/generate.bat
+++
@@ -1,7 +0,0 @@
-call vcvarsall.bat x64
-call vcvars64.bat
-set "PATH=%cd%/windows-x86_64;%PATH%"
-call java -jar javacpp.jar com/helixd2s/jivix/VectorTest.java -nocompile -header
-call java -jar javacpp.jar com/helixd2s/jivix/JiviXCore.java  -nocompile -header
-call java -jar javacpp.jar com/helixd2s/jivix/JiviXBase.java  -nocompile 
-pause
--- src/main/java/jnijavacpp.cpp
+++
@@ -1,1 +0,0 @@
-source/jnijavacpp.cpp
